07958a2b0b33e515f9f9bb7b155e50612a4f5837
Charles Ferreira <charles.dev@live.com>
2016-11-21 11:14:37 -0200

    - Criação do skybox - Organização de diretórios

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var speed = this.speed;
+        if (Input.GetKey("left shift") || Input.GetKey("right shift"))
+            speed *= 5f;
+
+        var horz = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        var vert = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.Rotate(Vector3.up, horz, Space.Self);
+        transform.Rotate(Vector3.right, vert, Space.Self);
+    }
+}
